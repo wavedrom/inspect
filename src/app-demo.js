e6aa7c1e5412f229ea295bcc138714a0602973fc
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-10-03 15:03:55 -0700

    refactoring

--- src/app-demo.js
+++ src/app-demo.js
@@ -3,6 +3,8 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const reEl = require('../lib/re-el.js');
+
 const $ = React.createElement;
 // const {useState} = React;
 
@@ -21,41 +23,12 @@ const zooData = {
         {type: 'reg',  size: 32, name: 'dat', id: 'Y'}
       ]
     }]
+  }, {
+    type: 'task', name: 'main_task'
   }]
 };
 
-function Var (props) {
-  return $('div', {}, props.type, props.size, props.name);
-}
-
-function El (props) {
-  return $('div',
-    {
-      'data-depth': props.depth,
-      style: {
-        position: 'absolute',
-        left: 0,
-        // top: 18 * props.depth,
-        height: 18,
-        width: '100%'
-      }
-    },
-    $('div',
-      {
-        style: {
-          transform: 'translateX(calc(2 * var(--indentation-size)))'
-        }
-      },
-      props.name,
-      (props.vars || []).map(e =>
-        $(Var, e)
-      ),
-      (props.scopes || []).map(e =>
-        $(El, Object.assign({depth: props.depth + 1}, e))
-      )
-    )
-  );
-}
+const El = reEl(React);
 
 function App (props) {
   return $('div', {}, $(El, Object.assign({depth: 0}, props)));
